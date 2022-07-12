f451876dad2a0eb92563c52bc49b8b95cf022e49
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2022-07-13 02:10:13 +0300

    Add AutoMapper & CRUD operations

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,32 +1,36 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-
-        }
+     
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivitiy(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id=id});
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditActivity(Guid id,Activity activity)
+        {
+            activity.Id=id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity=activity}));
         }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -15,6 +15,8 @@ using Microsoft.OpenApi.Models;
 using Persistence;
 using MediatR;
 using Application.Activities;
+using AutoMapper;
+using Application.Core;
 
 namespace API
 {
@@ -48,6 +50,7 @@ namespace API
                 });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
