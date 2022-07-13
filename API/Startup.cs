b5f0f1f0290f1ed82e4932b5178d1c2831094bcd
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2022-07-13 14:22:07 +0300

    Before Autofac extensions

--- API/Startup.cs
+++ API/Startup.cs
@@ -17,6 +17,7 @@ using MediatR;
 using Application.Activities;
 using AutoMapper;
 using Application.Core;
+using API.Extensions;
 
 namespace API
 {
@@ -29,28 +30,11 @@ namespace API
 
         }
 
-
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt=>{
-                opt.AddPolicy("CorsPolicy",policy=>{
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddApplicationServices(_config);   
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
