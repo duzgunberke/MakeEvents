1d3480e2b5c49549a945f22144af61363f9e78b0
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-01-09 04:40:50 +0300

    update the components according to mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { Container, Header, List } from 'semantic-ui-react';
+import { Button, Container, Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
@@ -7,6 +7,7 @@ import {v4 as uuid} from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
+import {observer} from 'mobx-react-lite';
 
 function App() {
   const {activityStore}=useStore();
@@ -14,36 +15,13 @@ function App() {
   const [activities,setActivities]=useState<Activity[]>([]);
   const [selectedActivity,setSelectedActivity]=useState<Activity | undefined>(undefined);
   const [editMode,setEditMode]=useState(false);
-  const[loading,setLoading]=useState(true);
   const [submitting,setSubmitting]=useState(false);
 
   useEffect(()=>{
-    agent.Activities.list().then(response=>{
-      let activities:Activity[] = [];
-      response.forEach(activity=>{
-        activity.date=activity.date.split('T')[0];
-        activities.push(activity);
-      })
-    setActivities(activities);
-    setLoading(false);
-    })
-  },[])
+    activityStore.loadActivities();
+  },[activityStore])
 
-  function handleSelectActivity(id:string){
-    setSelectedActivity(activities.find(x=>x.id===id));
-  }
 
-  function handleCancelSelectActivity(){
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?:string){
-    id ? handleSelectActivity(id):handleCancelSelectActivity();
-    setEditMode(true);
-  }
-  function handleFormClose(){
-    setEditMode(false);
-  }
   function handleCreateOrEditActivity(activity:Activity){
     setSubmitting(true);
     if(activity.id){
@@ -72,21 +50,14 @@ function App() {
     })
   }
 
-  if(loading) return <LoadingComponent content="Yükleniyor..." />
+  if(activityStore.loadingInitial) return <LoadingComponent content="Yükleniyor..." />
 
   return (
     <Fragment>
-      <NavBar openForm={handleFormOpen}/>
+      <NavBar />
       <Container style={{marginTop:'7em'}}>
-        <h2>{activityStore.title}</h2>
        <ActivityDashboard 
-       activities={activities} 
-       selectedActivity={selectedActivity}
-       selectActivity={handleSelectActivity}
-       cancelSelectActivity={handleCancelSelectActivity}
-       editMode={editMode}
-       openForm={handleFormOpen}
-       closeForm={handleFormClose}
+       activities={activityStore.activities} 
        createOrEdit={handleCreateOrEditActivity}
        deleteActivity={handleDeleteActivity}
        submitting={submitting}
@@ -96,4 +67,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,43 +4,36 @@ import { Activity } from '../../../app/models/activity';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityList from './ActivityList';
 import ActivityForm from '../form/ActivityForm';
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
 
 interface Props{
     activities: Activity[];
-    selectedActivity:Activity | undefined;
-    selectActivity:(id:string) =>void;
-    cancelSelectActivity:()=>void;
-    editMode:boolean;
-    openForm:(id:string)=>void;
-    closeForm:()=>void;
     createOrEdit:(activity:Activity)=>void;
     deleteActivity:(id:string)=>void;
     submitting:boolean;
 }
 
-export default function ActivityDashboard({activities,selectedActivity,
-  selectActivity,cancelSelectActivity,editMode,openForm,
-  closeForm,createOrEdit,deleteActivity,submitting}:Props) {
+export default observer(function ActivityDashboard({activities,createOrEdit,deleteActivity,submitting}:Props) {
+  
+  const{activityStore}=useStore();
+  const{selectedActivity,editMode}=activityStore;
+
   return (
     <Grid>
         <GridColumn width='10'>
             <ActivityList 
             activities={activities} 
-            selectActivity={selectActivity} 
             deleteActivity={deleteActivity}
             submitting={submitting}
             />
         </GridColumn>
         <GridColumn width='6'>
           {selectedActivity && !editMode &&
-          <ActivityDetails 
-          activity={selectedActivity} 
-          cancelSelectActivity={cancelSelectActivity}
-          openForm={openForm}
-          />}
+          <ActivityDetails />}
           {editMode &&
-          <ActivityForm submitting={submitting} closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+          <ActivityForm submitting={submitting} createOrEdit={createOrEdit}/>}
         </GridColumn>
     </Grid>
   )
-}
+})
