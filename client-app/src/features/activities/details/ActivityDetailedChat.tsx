eae6f75491393d6016f4ee42b3b728fc65b10c58
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-01-31 04:41:02 +0300

    refactor-ui-components

---
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ActiviDetailedChat(){
+    return (
+        <h1>Chat</h1>
+    )
+}
---
+++ client-app/src/features/activities/details/ActivityDetailedInfo.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ActiviDetailedInfo(){
+    return (
+        <h1>Info</h1>
+    )
+}
