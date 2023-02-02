97b5404d1efcd43f3bab130f9b97f37ff276bab3
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-02-02 16:05:51 +0300

    feat(all): error page

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,7 +1,9 @@
-import { createBrowserRouter, RouteObject } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouteObject } from "react-router-dom";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import ActivityForm from "../../features/activities/form/ActivityForm";
+import NotFound from "../../features/errors/NotFound";
+import TestErrors from "../../features/errors/TestError";
 import HomePage from "../../features/home/HomePage";
 import App from "../layout/App";
 
@@ -14,6 +16,9 @@ export const routes: RouteObject[] = [
             { path: 'activities/:id', element: <ActivityDetails /> },
             { path: 'createActivity', element: <ActivityForm key='create'/> },
             { path: 'manage/:id', element: <ActivityForm key='manage'/> },
+            { path: 'errors', element: <TestErrors /> },
+            { path: 'not-found', element: <NotFound /> },
+            { path: '*', element: <Navigate replace to='/not-found' /> },
         ]
     }
 ]
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/layout/styles.css';
 import 'react-calendar/dist/Calendar.css'
+import 'react-toastify/dist/ReactToastify.min.css';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
