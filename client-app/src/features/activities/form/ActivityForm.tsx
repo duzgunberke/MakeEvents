5ee0c87794a048e24c0a1c78fa4fec5f110df89a
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-02-04 00:53:14 +0300

    feat(form): datepicker

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -12,6 +12,7 @@ import MyTextInput from '../../../app/common/form/MyTextInput';
 import MyTextArea from '../../../app/common/form/MyTextArea';
 import MySelectInput from '../../../app/common/form/MySelectInput';
 import { categoryOptions } from '../../../app/common/options/categoryOptions';
+import MyDateInput from '../../../app/common/form/MyDateInput';
 
 export default observer(function ActivityForm() {
 
@@ -73,7 +74,13 @@ export default observer(function ActivityForm() {
             <MyTextInput placeholder='Title' name='title' />
             <MyTextArea  placeholder="Description" name="description" rows={3}  />
             <MySelectInput options={categoryOptions} placeholder="Category" name="category" />
-            <MyTextInput placeholder="Date" name="date" />
+            <MyDateInput 
+                placeholderText="Date" 
+                name="date"
+                showTimeSelect
+                timeCaption='time'
+                dateFormat='MMMM d, yyyy h:mm aa'
+            />
             <MyTextInput placeholder="City" name="city" />
             <MyTextInput placeholder="Venue" name="venue" />
             <Button loading={loading} floated="right" positive type="submit" content="Submit" />
