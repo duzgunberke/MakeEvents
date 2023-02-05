7297cef083d143fff1337e71c62c36e655b87e4b
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-02-06 01:39:23 +0300

    feat(persistance): Users operations

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,7 @@ namespace API
         {
             services.AddControllers();
             services.AddApplicationServices(_config);   
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
