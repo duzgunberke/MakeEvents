e0d2250b569c6844ddd120e9622665d854731142
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-02-08 00:59:33 +0300

    feat(backend): User claims

--- API/Startup.cs
+++ API/Startup.cs
@@ -19,6 +19,9 @@ using AutoMapper;
 using Application.Core;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API
 {
@@ -34,7 +37,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(opt=>{
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            });
             services.AddApplicationServices(_config);   
             services.AddIdentityServices(_config);
         }
@@ -55,6 +61,8 @@ namespace API
             app.UseRouting();
 
             app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
