673a8540d60dc78b9d0b6b64e3a564bdca4329b3
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-02-16 03:03:08 +0300

    reafactor(domain): photo

--- Domain/Photo.cs
+++ Domain/Photo.cs
@@ -5,8 +5,5 @@ namespace Domain
         public string Id { get; set; }
         public string Url { get; set; }
         public bool IsMain { get; set; }
-        public string PublicId { get; set; }
-        public AppUser AppUser { get; set; }
-        public string AppUserId { get; set; }
     }
 }
---
+++ Persistence/Migrations/20230215235846_refactorphoto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class refactorphoto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Photos");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "Photos",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
