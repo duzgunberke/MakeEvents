2048e0aab47266f9fa78ad20f96362becd9031f2
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-02-18 03:12:15 +0300

    feat(frontend): Profile store & components

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -94,6 +94,7 @@ const Profiles ={
     },
     setMainPhoto:(id:string)=>requests.post(`/photos/${id}/setMain`,{}),
     deletePhoto:(id:string)=>requests.del(`/photos/${id}`),
+    updateProfile:(profile:Partial<Profile>)=>requests.put(`/profiles`,profile),
 }
 
 const agent ={
