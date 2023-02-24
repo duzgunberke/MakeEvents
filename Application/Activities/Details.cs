fa99b4eb6517934e718dee58282257bfce62357b
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-02-24 16:14:43 +0300

    feat(application): follower CQRS

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,5 +1,6 @@
 using Application.Activities.DTOs;
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Domain;
@@ -20,17 +21,19 @@ namespace Application.Activities
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
                 _context = context;
                 _mapper = mapper;
+                _userAccessor = userAccessor;
             }
 
             public async Task<Result<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities
-                .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider,new {currentUsername = _userAccessor.GetUsername()})
                 .FirstOrDefaultAsync(x => x.Id == request.Id);
 
                 return Result<ActivityDto>.Success(activity);
--- Application/Followers/List.cs
+++ Application/Followers/List.cs
@@ -46,7 +46,7 @@ namespace Application.Followers
                     case "following":
                         profiles = await _context.UserFollowings.Where(x => x.Observer.UserName == request.Username)
                             .Select(x => x.Target)
-                            .ProjectTo<Profiles.Profile>(_mapper.ConfigurationProvider)
+                            .ProjectTo<Profiles.Profile>(_mapper.ConfigurationProvider,new {currentUsername = _userAccessor.GetUsername()})
                             .ToListAsync();
                         break;
                 }
