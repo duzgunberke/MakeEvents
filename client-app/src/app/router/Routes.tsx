090d99f074e0359c601c4f2ca026e9307a1ef6c0
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-03-19 15:23:51 +0300

    feat(client): client side private routes

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -9,19 +9,21 @@ import HomePage from "../../features/home/HomePage";
 import ProfilePage from "../../features/profiles/ProfilePage";
 import LoginForm from "../../features/users/LoginForm";
 import App from "../layout/App";
+import RequireAuth from "./RequireAuth";
 
 export const routes: RouteObject[] = [
     {
         path: '/',
         element: <App />,
         children: [
-            { path: 'activities', element: <ActivityDashboard /> },
-            { path: 'activities/:id', element: <ActivityDetails /> },
-            { path: 'createActivity', element: <ActivityForm key='create'/> },
-            { path: 'manage/:id', element: <ActivityForm key='manage'/> },
-            { path: 'profiles/:username', element: <ProfilePage /> },
-            { path: 'login', element: <LoginForm /> },
-            { path: 'errors', element: <TestErrors /> },
+            {element: <RequireAuth />, children: [
+                { path: 'activities', element: <ActivityDashboard /> },
+                { path: 'activities/:id', element: <ActivityDetails /> },
+                { path: 'createActivity', element: <ActivityForm key='create'/> },
+                { path: 'manage/:id', element: <ActivityForm key='manage'/> },
+                { path: 'profiles/:username', element: <ProfilePage /> },
+                { path: 'errors', element: <TestErrors /> },
+            ]},
             { path: 'not-found', element: <NotFound /> },
             { path: 'server-error', element: <ServerError /> },
             { path: '*', element: <Navigate replace to='/not-found' /> },
