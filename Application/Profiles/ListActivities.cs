c24a288cd0944c3355f99554adbc2e1eb2d334cb
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-03-20 23:43:58 +0300

    feat(backend): switch to postgre db & datetime utc

--- Application/Profiles/ListActivities.cs
+++ Application/Profiles/ListActivities.cs
@@ -33,10 +33,10 @@ namespace Application.Profiles
                 .AsQueryable();
                 query = request.Predicate switch
                 {
-                    "past" => query.Where(a => a.Date <= DateTime.Now),
+                    "past" => query.Where(a => a.Date <= DateTime.UtcNow),
                     "hosting" => query.Where(a => a.HostUsername ==
                    request.Username),
-                    _ => query.Where(a => a.Date >= DateTime.Now)
+                    _ => query.Where(a => a.Date >= DateTime.UtcNow)
                 };
                 var activities = await query.ToListAsync();
                 return Result<List<UserActivityDto>>.Success(activities);
